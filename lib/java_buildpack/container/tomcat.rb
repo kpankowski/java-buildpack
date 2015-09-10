f9e96c753f582e946759ead513fea0820ea0e033
Krzysztof Pankowski <krzysztof.pankowski@gmail.com>
2015-09-10 15:42:24 +0200

    Limit tomcat threads (test commit)

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -36,7 +36,8 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::ModularComponent#command)
       def command
         @droplet.java_opts.add_system_property 'http.port', '$PORT'
-
+        @droplet.java_opts.add_system_property 'tomcat.maxThreads', '35'
+        @droplet.java_opts.add_system_property 'tomcat.minSpareThreads', '35'
         [
           @droplet.environment_variables.as_env_vars,
           @droplet.java_home.as_env_var,
